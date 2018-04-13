7a1f80a6017bb84541c91cb3a4475304b9e46476
syarif <syarif.hidayatullah@stud.unibas.ch>
2018-04-13 09:15:54 +0200

    Blatt3Aufgabe1

--- Blatt3/src/main/java/projection/PinholeProjection.java
+++ Blatt3/src/main/java/projection/PinholeProjection.java
@@ -10,30 +10,38 @@ import utils.Matrix4;
 public class PinholeProjection extends Projection {
 
 	protected Matrix4 camera, view, projection;
-	
+
 	public PinholeProjection(int width, int height){
 		super(width, height);
 		initializeCamera();
 		initializeView();
 		projection = Matrix4.multiply(camera, view);
 	}
-	
+
 	private void initializeCamera(){
 		camera = new Matrix4();
 		//TODO: Blatt 3
+		camera.set(0,0,width);
+		camera.set(1,1, height);
+		camera.set(0,2,width/2);
+		camera.set(1,2,height/2);
 	}
-	
+
 	private void initializeView(){
 		view = new Matrix4();
 	}
-	
+
 	@Override
 	public Vector3 project(Vector3 pt) {
-		
+
 		//TODO: Blatt 3
-		return new Vector3(0,0,0);
+		Vector4 vector4 = new Vector4(pt.x,pt.y,pt.z,1);
+
+		Vector4 result = projection.multiply(vector4);
+
+		return new Vector3(result.x/result.z,result.y/result.z,result.z);
 	}
-	
+
 	@Override
 	public Matrix4 getMatrix(){
 		return projection;
@@ -42,29 +50,29 @@ public class PinholeProjection extends Projection {
 	public Matrix4 getView(){
 		return view;
 	}
-	
+
 	public void setView(Matrix4 m){
 		view = m;
 		projection = Matrix4.multiply(camera, view);
 	}
-	
+
 	public void setCamera(Matrix4 m){
 		camera = m;
 		projection = Matrix4.multiply(camera, view);
 	}
-	
+
 	public Vector4 getEye(){
 		Matrix4 ti = new Matrix4(projection);
 		ti = ti.inverted();
-		
+
 		Vector4 e = ti.multiply(new Vector4(0,0,0,1));
 		e = e.times(1.0/e.w);
-		
+
 		return e;
 	}
-	
+
 	public Matrix4 getViewMatrixOfLightSource(PointLight lightSource){
-		
+
 		//Blatt 5, Aufgabe 1 a)
 		return new Matrix4();
 	}
--- Blatt3/src/main/java/projection/Projection.java
+++ Blatt3/src/main/java/projection/Projection.java
@@ -8,37 +8,73 @@ import utils.Vector4;
 public abstract class Projection {
 
 	public static Matrix4 getRotationX(double angle) {
+
 		//TODO: Blatt 3
-		return new Matrix4();
+		double cosi = Math.cos(angle);
+		double sini = Math.sin(angle);
+
+		Matrix4 matrix4 = new Matrix4();
+		matrix4.set(1,1,cosi);
+		matrix4.set(2,2,cosi);
+		matrix4.set(2,1,sini);
+		matrix4.set(1,2,sini*-1);
+		return matrix4;
 	}
 
 	public static Matrix4 getRotationY(double angle) {
 		//TODO: Blatt 3
-		return new Matrix4();
+		double cosi = Math.cos(angle);
+		double sini = Math.sin(angle);
+
+		Matrix4 matrix4 = new Matrix4();
+		matrix4.set(0,0,cosi);
+		matrix4.set(2,2,cosi);
+		matrix4.set(2,0,sini*-1);
+		matrix4.set(0,2,sini);
+		return matrix4;
 	}
 
 	public static Matrix4 getRotationZ(double angle) {
 		//TODO: Blatt 3
-		return new Matrix4();
+
+		double cosi = Math.cos(angle);
+		double sini = Math.sin(angle);
+
+		Matrix4 matrix4 = new Matrix4();
+		matrix4.set(0,0,cosi);
+		matrix4.set(1,1,cosi);
+		matrix4.set(1,0,sini);
+		matrix4.set(0,1,sini*-1);
+		return matrix4;
 	}
 
 	public static Matrix4 getTranslation(Vector3 trans) {
 		//TODO: Blatt 3
+
+		Matrix4 matrix4 = new Matrix4();
+		matrix4.set(0,4,trans.x);
+		matrix4.set(1,4,trans.y);
+		matrix4.set(2,4,trans.z);
 		return new Matrix4();
 	}
 
 	public static Matrix4 getScaling(Vector3 scale) {
 		//TODO: Blatt 3
-		return new Matrix4();
+
+		Matrix4 matrix4 = new Matrix4();
+		matrix4.set(0,0,scale.x);
+		matrix4.set(1,1,scale.y);
+		matrix4.set(2,2,scale.z);
+		return matrix4;
 	}
 
 	protected int width, height;
-	
+
 	public Projection(int width, int height){
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	public abstract Vector3 project(Vector3 pt);
 	public abstract Vector4 getEye();
 	public abstract Matrix4 getViewMatrixOfLightSource(PointLight pointLight);
