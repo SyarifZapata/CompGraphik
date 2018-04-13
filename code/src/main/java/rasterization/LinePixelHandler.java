f4ddfe7f3bf600978d9ddf46398904d754b70d5e
syarif <syarif.hidayatullah@stud.unibas.ch>
2018-04-13 10:04:44 +0200

    delete Folder Code

--- code/src/main/java/rasterization/LinePixelHandler.java
+++
@@ -1,9 +0,0 @@
-package rasterization;
-
-import utils.Vector2;
-
-public interface LinePixelHandler {
-	
-	public void handleLinePixel(int x, int y, Vector2 va, Vector2 vb);
-
-}
--- code/src/main/java/rasterization/TrianglePixelHandler.java
+++
@@ -1,9 +0,0 @@
-package rasterization;
-
-import utils.BarycentricCoordinates;
-
-public interface TrianglePixelHandler {
-	
-	public void handleTrianglePixel(int x, int y, BarycentricCoordinates triCoords);
-
-}
