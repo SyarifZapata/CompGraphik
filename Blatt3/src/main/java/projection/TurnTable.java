17e7ecd9ca47c5489d037519935767511ec5f499
syarif <syarif.hidayatullah@stud.unibas.ch>
2018-04-15 15:54:43 +0200

    zoom implemented

--- Blatt3/src/main/java/projection/TurnTable.java
+++ Blatt3/src/main/java/projection/TurnTable.java
@@ -33,15 +33,25 @@ public class TurnTable implements MouseListener, MouseMotionListener {
 	}
 	
 	private void updateView(double azimuth, double elevation) {
+		//System.out.println("UpdateView");
+		//System.out.println(translation);
 		currentView = buildViewMatrix(azimuth, elevation, translation);
+		//System.out.println(currentView);
 		setRendererViewMatrix(currentView);
+		//System.out.println("Updated");
 	}
 	
 	private Matrix4 buildViewMatrix(double azimuth, double elevation, Vector3 translation){
 		//TODO: Blatt 3, Aufgabe 2
 		Matrix4 zwischenRes = Projection.getRotationY(azimuth);
-		Matrix4 result = Matrix4.multiply(zwischenRes,Projection.getRotationZ(elevation));
-		currentView = Matrix4.multiply(result,Projection.getTranslation(translation));
+		//System.out.println(zwischenRes);
+		currentView = Matrix4.multiply(zwischenRes,Projection.getRotationZ(elevation));
+		//System.out.println(result);
+		//currentView = Matrix4.multiply(result,Projection.getTranslation(translation));
+		currentView.set(0,3,translation.x);
+		currentView.set(1,3,translation.y);
+		currentView.set(2,3,translation.z);
+		//System.out.println(currentView);
 		return currentView;
 	}
 	
@@ -59,7 +69,7 @@ public class TurnTable implements MouseListener, MouseMotionListener {
 				double lazimuth = handleAzimuth(e.getX(), 0);
 				double lelevation = handleElevation(e.getY(), 0);
 	
-				Matrix4 lrot = buildViewMatrix(lazimuth, lelevation, new Vector3(0, 0, 0));
+				Matrix4 lrot = buildViewMatrix(lazimuth, lelevation, new Vector3(0,0,0));
 				renderer.rotateLights(lrot);
 			}else{
 				azimuth = handleAzimuth(e.getX(), azimuth);
@@ -90,8 +100,19 @@ public class TurnTable implements MouseListener, MouseMotionListener {
 
 	private void zoom(boolean in){
 		//TODO: Blatt 3, Aufgabe 2
+
+
+		if(in){
+//			currentView.set(2,3, currentView.get(2,3)+zoomStep);
+			translation = translation.plus(new Vector3(0,0,zoomStep));
+
+		}else {
+//			currentView.set(2,3, currentView.get(2,3)-zoomStep);
+			translation = translation.minus(new Vector3(0,0,zoomStep));
+		}
 		
 		updateView(azimuth, elevation);
+		//System.out.println(currentView);
 		test.draw();
 	}
 	
@@ -107,7 +128,7 @@ public class TurnTable implements MouseListener, MouseMotionListener {
 		//TODO: Blatt 3, Aufgabe 2
 		double y = newMouseY - mouseY;
 		double ratio = (y/h)*Math.PI;
-		elevation = elevation - ratio;
+		elevation = elevation + ratio;
 		return elevation;
 	}
 	
