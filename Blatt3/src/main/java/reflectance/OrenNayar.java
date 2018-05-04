cea2a192ddf3a19ee7b02f4bcd960d9683c5418b
syarif <syarif.hidayatullah@stud.unibas.ch>
2018-05-04 13:06:39 +0200

    Aufgabe 5

--- Blatt3/src/main/java/reflectance/OrenNayar.java
+++ Blatt3/src/main/java/reflectance/OrenNayar.java
@@ -16,32 +16,25 @@ public class OrenNayar extends Brdf{
 
 	@Override
 	protected RGBA getRadiance(Vector3 toEye, Vector3 toLight, Vector3 n){
-		
+
 		//Blatt 4, Aufgabe 5
 
 		double a = 1 - 0.5 * (sigmaSquarred/(sigmaSquarred + 0.33));
 		double b = 0.45 * (sigmaSquarred/(sigmaSquarred + 0.09));
 
-		toEye = toEye.normalize();
-		toLight = toLight.normalize();
-		n = n.normalize();
-
-		double lNscalar = toLight.dot(n);
-		double vNscalar = toEye.dot(n);
+		double lNscalar = toLight.dot(n)/(toLight.length()* n.length());
+		double vNscalar = toEye.dot(n)/(n.length() * toEye.length());
 
 		double phiIn = Math.acos(lNscalar);
 		double phiOut = Math.acos(vNscalar);
-		System.out.println(phiIn);
-		System.out.println(phiOut);
 
 		double alpha = Math.max(phiIn,phiOut);
 		double beta = Math.min(phiIn,phiOut);
+
 		double tanBeta = Math.sin(beta)/ (Math.cos(beta) + 0.0001);
 
-		albedo = albedo.times(a + (b * Math.max(0,Math.cos(phiIn-phiOut) * Math.sin(alpha) * tanBeta)));
-		
-		
-		return albedo;
+		return albedo.times(a + (b * Math.max(0,Math.cos(phiIn-phiOut)) * Math.sin(alpha) * tanBeta));
+
 	}
 
 
